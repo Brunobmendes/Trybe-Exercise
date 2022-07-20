2cd69cdb48f0129ed32736b12c2cabcba147882e
BrunoPsai <Bruno.mendes2804@gmail.com>
2022-07-20 16:16:01 -0300

    para-fixar variaveis

---
+++ fundamentos/Bloco-04-javascript/dia01-javascript-primeiros-passos/para-fixar/variaveis/variaveis.js
@@ -0,0 +1,7 @@
+const myName = "Bruno";
+const birthCity = "Ibaiti";
+let birthYear = 2004;
+console.log (myName, birthCity, birthYear);
+birthYear = 2030;
+console.log (birthYear);
+birthCity = Araucaria;
