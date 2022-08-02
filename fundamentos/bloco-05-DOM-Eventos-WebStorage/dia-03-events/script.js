d61313733ced1b5fbb65bb2d59731879289fecd2
BrunoPsai <Bruno.mendes2804@gmail.com>
2022-08-01 23:08:35 -0300

    exercises 3,4,5,6 complete

--- fundamentos/bloco-05-DOM-Eventos-WebStorage/dia-03-events/script.js
+++ fundamentos/bloco-05-DOM-Eventos-WebStorage/dia-03-events/script.js
@@ -46,7 +46,7 @@ function addElementToParent(elementCreated, parentAttached) {
 for (let index = 0; index < decemberDaysList.length; index++) {
   //add all december days to calendar
   let createDays = document.createElement("li");
-  createDays.className += "day";
+  createDays.className += " day";
   createDays.innerText = decemberDaysList[index];
   addElementToParent(createDays, "#days");
   verifySpecialDay(decemberDaysList[index], createDays);
@@ -56,16 +56,17 @@ function verifyHoliday(dia, classeCriada) {
   //verifica se o dia é feriado e adiciona uma classe a ele
   for (const valor of specialDays.holiday) {
     if (dia === valor) {
-      classeCriada.className = "holiday";
+      classeCriada.className += " holiday";
     }
   }
 }
 function verifyFriday(dia, classeCriada) {
   //verifica se o dia é sexta e adiciona uma classe a ele
   for (const valor of specialDays.friday) {
-    if (dia === valor && dia !== 25) {
-      classeCriada.className = " friday";
+    if (dia === valor) {
+      classeCriada.className += " friday";
     }
+
   }
 }
 function verifySpecialDay(dia, classeCriada) {
@@ -74,20 +75,21 @@ function verifySpecialDay(dia, classeCriada) {
   verifyFriday(dia, classeCriada);
 }
 
-function createButton(buttonName) {
+function createButtonHoliday(buttonName) {
   //cria um botão com o nome passado
   let buttonHoliday = document.createElement("button");
   buttonHoliday.setAttribute("id", "btn-holiday");
   buttonHoliday.innerText += buttonName;
   addElementToParent(buttonHoliday, ".buttons-container");
-  corMudada("blue", "whitesmoke");
+  corMudadaHoliday("blue", "whitesmoke");
 }
 
-function corMudada(colorChanged, colorOriginal) {
+function corMudadaHoliday(colorChanged, colorOriginal) {
+  //pega os parâmetros da cor original e a cor que vai mudar e altera o background quando o botão é apertado
   const buttonHoliday = document.querySelector("#btn-holiday");
 
   buttonHoliday.addEventListener("click", function () {
-
+    //adiciona um evento de click ao botão de feriado que altera o background dele
     const indexFeriado = document.getElementsByClassName("holiday");
     for (let index = 0; index < indexFeriado.length; index++) {
       if (indexFeriado[index].style.backgroundColor !== colorChanged) {
@@ -98,5 +100,56 @@ function corMudada(colorChanged, colorOriginal) {
     }
   });
 }
+function createButtonFriday(buttonName) {
+  //cria um botão com o nome passado
+  let buttonFriday = document.createElement("button");
+  buttonFriday.setAttribute("id", "btn-friday");
+  buttonFriday.innerText += buttonName;
+  addElementToParent(buttonFriday, ".buttons-container");
+  corMudadaFriday(buttonName, );
+}
 
-createButton("Feriados");
+function corMudadaFriday(textChanged, textOriginal) {
+  //pega os parâmetros do text original e o text que vai mudar e altera o text quando o botão é apertado
+  const buttonFriday = document.querySelector("#btn-friday");
+  
+  buttonFriday.addEventListener("click", function () {
+    //adiciona um evento de click ao botão da sexta que altera o inner text dele
+    const indexSexta = document.getElementsByClassName("friday");
+    for (let index = 0; index < indexSexta.length; index++) {
+      if (indexSexta[index].innerText !== textChanged) {
+        indexSexta[index].innerText = textChanged;
+      } else {
+        indexSexta[index].innerText = specialDays.friday[index];
+      }
+    }
+  });
+}
+
+
+//funções para dar zoom
+function zoom(){
+  mouseIn()
+  mouseOut()
+}
+function mouseIn(){
+  let days = document.querySelector("#days")
+  days.addEventListener("mouseover", function(event){
+    event.target.style.fontSize = "30px"
+    event.target.style.fontSize = "300"
+  })
+}
+function mouseOut(){
+  let days = document.querySelector("#days")
+  days.addEventListener("mouseout", function(event){
+    event.target.style.fontSize = "20px"
+    event.target.style.fontSize = "200"
+  })
+}
+
+
+
+createButtonHoliday("Feriados");
+createButtonFriday("Sextou")
+zoom()
+
